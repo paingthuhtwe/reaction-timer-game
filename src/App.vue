0f0ac98e116a7e58f4d344d8025b8a9830c7006c
Paing Thu Htwe <paingthuhtwe.official@gmail.com>
2023-05-11 17:29:57 +0630

    created blogpage

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-center font-bold text-4xl text-sky-800 select-none">
+    How fast can you catch me?
+  </h1>
+  <button
+    class="bg-blue-500 px-4 py-1.5 font-semibold text-slate-100 text-xl mt-9 rounded-md enabled:hover:bg-blue-600 disabled:opacity-75"
+    @click="start"
+    :disabled="isPlaying"
+  >
+    Play
+  </button>
+  <div v-if="isPlaying">
+    <BlogPage :delay="delay" @click="stop" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import BlogPage from "./components/BlogPage.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = !this.isPlaying;
+      this.delay = 2000 + Math.random() * 5000;
+    },
+    stop() {
+      this.isPlaying = !this.isPlaying;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    BlogPage,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/BlogPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="blog w-96 h-64 bg-teal-400 hover:bg-teal-500 cursor-pointer mx-auto my-12 rounded-lg text-xl font-bold text-slate-600 flex items-center justify-center"
+    @click="stopPlaying"
+    v-if="showBlog"
+  >
+    Click Here
+    {{ delay }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showBlog: false,
+    };
+  },
+  props: ["delay"],
+  methods: {
+    stopPlaying() {
+      this.$emit("stop");
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlog = !this.showBlog;
+    }, this.delay);
+  },
+  updated() {
+    this.showBlog = !this.showBlog;
+  },
+};
+</script>
+
+<style>
+</style>
